f37106abc1f27ce9d5c3eab2571e158bf87685bf
srinidhi2010 <109321775+srinidhi2010@users.noreply.github.com>
2023-09-30 16:31:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game()
+}
+function setup(){
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+}
+
+
+
+
+
+
